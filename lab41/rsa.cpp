9c494fa5b156a43f82706355ef7444653f93a205
platosha <15platosha@gmail.com>
2021-12-08 13:36:02 +0300

    done rsa

--- lab41/rsa.cpp
+++ lab41/rsa.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-unsigned int RSA::binpow(unsigned int a, unsigned int n, unsigned int m) {
+unsigned int RSA::binpow(unsigned int a, unsigned int n, unsigned int m)
+{
     unsigned int res = 1;
     while (n) {
         if (n & 1) {
@@ -17,24 +18,26 @@ unsigned int RSA::binpow(unsigned int a, unsigned int n, unsigned int m) {
     return res % m;
 }
 
-unsigned int RSA::encode(unsigned char symbol) {
-    auto encoded = binpow(symbol, e, n);
-    return encoded;
-}
-
 void RSA::initPublicKey(vector<unsigned int> publicKey)
 {
     e = publicKey[0];
     n = publicKey[1];
 }
 
-unsigned char RSA::decode(unsigned int symbol) {
-    auto decoded = binpow(symbol, d, n);
-    return static_cast<unsigned char>(decoded);
-}
-
-void RSA::initPrivateKey(std::vector<unsigned int> privateKey)
+void RSA::initPrivateKey(vector<unsigned int> privateKey)
 {
     d = privateKey[0];
     n = privateKey[1];
+}
+
+unsigned int RSA::encode(unsigned char symbol)
+{
+    unsigned int enSymbol = binpow(symbol, e, n);
+    return enSymbol;
+}
+
+unsigned char RSA::decode(unsigned int symbol)
+{
+    unsigned int deSymbol = binpow(symbol, d, n);
+    return static_cast<unsigned char>(deSymbol);
 }
--- lab41/rsa.h
+++ lab41/rsa.h
@@ -7,15 +7,16 @@ class RSA {
 public:
     void initPublicKey(std::vector<unsigned int> publicKey);
     void initPrivateKey(std::vector<unsigned int> privateKey);
+
     unsigned int encode(unsigned char symbol);
     unsigned char decode(unsigned int symbol);
 
 private:
-    unsigned int e{};
-    unsigned int d{};
-    unsigned int n{};
+    unsigned int e;
+    unsigned int d;
+    unsigned int n;
 
-    unsigned int binpow(unsigned int a, unsigned int n, unsigned int m);
+    unsigned int binpow(unsigned int a, unsigned int n, unsigned int mod);
 };
 
 #endif // RSA_H
